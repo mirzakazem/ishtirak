cb200cbc95978b37e5d87ab34b220a241c39261b
Kazem Seblani <kazem.seblani@gmail.com>
2020-08-18 13:48:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ session_start();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="برنامج اشتراك">
+<meta name="keyword" content="ishtirak, ishtirak program, ishtirak software, ishtirak application, اشتراك,برنامج اشتراك">	
 	
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 
